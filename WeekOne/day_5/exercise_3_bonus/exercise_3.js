cd34532d8429816c37fed2649113a1870048af55
Imad Arain <arain.imad@gmail.com>
2020-01-13 09:55:21 +0100

    Stickman HTML Bonus Ezxercise

---
+++ WeekOne/day_5/exercise_3_bonus/exercise_3.js
@@ -0,0 +1,17 @@
+// Write a constructor called Countdown that accepts a single argument - the number
+//  of seconds to count down. It should be possible to call the start method of
+//  instances of Countdown to initiate the countdown. Once the countdown starts,
+//  it should count down to zero starting with the number that was passed to the
+//  constructor and logging each number to the console with a one second delay.
+
+
+
+
+
+
+
+
+
+//Testing Code
+var countdown = new Countdown(5);
+countdown.start();
