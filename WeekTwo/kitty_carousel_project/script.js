90e1a4bca1d64da11eb36f95f4e357f91b90949e
Imad Arain <arain.imad@gmail.com>
2020-01-17 17:07:41 +0100

    Finished Kitty Carousel

--- WeekTwo/kitty_carousel_project/script.js
+++ WeekTwo/kitty_carousel_project/script.js
@@ -1,70 +1,72 @@
 (function() {
-
+    ////////////////////--VARIABLES--////////////////////
     var kitties = document.getElementsByTagName('img');
+    var dots = document.getElementsByClassName('dot');
+    var timer;
     var active = 0;
+    var transitioningRightNow;
 
+    ////////////////////--EVENT LISTENER for Touch Right (BONUS)--////////////////////
+
+
+    ////////////////////--EVENT LISTENER ON TOUCH (BONUS)--////////////////////
+    document.addEventListener('touchmove', function() {
+        if (transitioningRightNow){
+            return;
+        } else {
+            clearTimeout(timer);
+            var touchNext = active +1;
+            moveKitties(touchNext);
+        }
+
+
+    });
+
+    ////////////////////--EVENT LISTENER ON TRANSITION--////////////////////
     document.addEventListener('transitionend', function(e){
-
         if (e.target.classList.contains("offscreen-left")) {
-
             e.target.classList.remove("offscreen-left");
-
-            setTimeout(moveKitties, 1000);
+            timer = setTimeout(moveKitties, 5000);
+            transitioningRightNow = false;
         }
     });
 
-    function moveKitties () {
+    ////////////////////--EVENT LISTENER ON CLICKS--////////////////////
+
+    Array.prototype.slice.call(dots).forEach(function(dot, index) {
+        dot.addEventListener('click', function (e) {
+
+            if (e.target.classList.contains("on")) {
+                return;
+            }
+            if (transitioningRightNow) {
+                return;
+            }
+            clearTimeout(timer);
+            moveKitties(index);
+        });
+    });
+
+    ////////////////////--TRANSITIONING MY KITTIES--////////////////////
+    function moveKitties (next) {
+        transitioningRightNow = true;
         kitties[active].classList.remove('onscreen');
         kitties[active].classList.add('offscreen-left');
+        dots[active].classList.remove('on');
 
-        active++;
-
-        if (active == kitties.length) {
-            active = 0;
+        if (typeof next == "undefined") {
+            active++;
+            if (active >= kitties.length) {
+                active = 0;
+            }
+        } else {
+            active = next;
         }
-        kitties[active].classList.add('onscreen');
 
+        kitties[active].classList.add('onscreen');
+        dots[active].classList.add('on');
     }
 
-    setTimeout(moveKitties, 1000);
+    timer = setTimeout(moveKitties, 5000);
 
 }());
-
-
-
-
-// Version 2
-// (function() {
-//
-//     var kitties = document.getElementsByTagName('img');
-//     var active = 0;
-//
-//     setTimeout(moveKitties, 1000);
-//
-//     document.addEventListener('transitionend', function(e){
-//
-//         if (e.target.classList.contains("offscreen-left")) {
-//
-//             e.target.classList.remove("offscreen-left");
-//
-//             setTimeout(moveKitties, 1000);
-//         }
-//     });
-//
-//     function moveKitties (inLine) {
-//         kitties[active].classList.remove('onscreen');
-//         kitties[active].classList.add('offscreen-left');
-//
-//         if(typeof inLine === 'undefined'){
-//             active++;
-//             if ( active>= kitties.length){
-//                 active = 0;
-//             }
-//         } else {
-//             active = inLine;
-//         }
-//
-//         kitties[active].classList.add('onscreen');
-//     }
-//
-// }());
