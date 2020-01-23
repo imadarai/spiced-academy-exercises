ee2d02e6ac2a905378e2a9f397dc3348b37a5a9b
Imad Arain <arain.imad@gmail.com>
2020-01-23 17:51:17 +0100

    fixed Translate Numbers to german exercise

--- Week_3/day_3/numbers_to_german/script.js
+++ Week_3/day_3/numbers_to_german/script.js
@@ -2,9 +2,6 @@ console.log("Hello Beautiful");
 
 (function() {
 
-    var display = $('#display');
-    var germanNumbers = ['EIN !', 'ZWEI !', 'DREI !', 'VIER !', 'FUNF !', 'SECHS !', 'SEIBEN !', 'ACHT !', 'NUEN !', 'ZEHN !'];
-
     function askForNumber() {
         var num = prompt('Please enter a number between 1 and 10');
         if (num >= 1 && num <= 10 && num == parseInt(num)) {
@@ -13,21 +10,24 @@ console.log("Hello Beautiful");
         throw new Error('Bad number');
     }
 
-    transalateNumberToGerman();
+    var $display = $('#display');
+    var germanNumbers = ['EIN !', 'ZWEI !', 'DREI !', 'VIER !', 'FUNF !', 'SECHS !', 'SEIBEN !', 'ACHT !', 'NUEN !', 'ZEHN !'];
+
+
     function transalateNumberToGerman () {
         try {
             var input = askForNumber();
-            var output = germanNumbers[input - 1];
-            display.html(output);
-        }
-        catch(e){
-            console.log('Maybe you should Learn English first .... We asked for 1 through 10 ...');
-            transalateNumberToGerman();
+            var output = germanNumbers[input -1];
+            return output;
+
+        } catch (err){
+            console.log('Error', err);
+            return transalateNumberToGerman ();
         }
     }
 
-    setInterval(function(){
-        window.location.reload(true);
-    }, 5000);
 
-}());
+    $display.html(transalateNumberToGerman());
+
+
+})();
