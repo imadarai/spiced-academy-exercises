a9637876769626803ee2f323d44049c8629297bc
Imad Arain <arain.imad@gmail.com>
2020-01-30 01:59:18 +0100

    GitHub API Exercise without Bonus

---
+++ Week_4/day_3_github_api/github_api/script.js
@@ -0,0 +1,75 @@
+console.log("Hello Beautiful!");
+
+(function() {
+    //////////////////DO NOT TOUCH /////////////////////////
+    Handlebars.templates = Handlebars.templates || {};
+    var templates = document.querySelectorAll('script[type="text/x-handlebars-template"]');
+    Array.prototype.slice.call(templates).forEach(function(script) {
+        Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
+    });
+    //////////////////DO NOT TOUCH /////////////////////////
+
+    let username;
+    let password;
+    let userToSearch;
+    let baseUrl;
+    let endpoint;
+    let $usernameField = $('input[name="username"]');
+    let $passwordField = $('input[name="password"]');
+
+    $('button').on('click', function() {
+        username = $usernameField.val();
+        password = $passwordField.val();
+        userToSearch = $('input[name="user-to-search"]').val();
+
+        baseUrl = "https://api.github.com";
+        endpoint = `/users/${userToSearch}/repos`;
+
+        $.ajax({
+            url: baseUrl + endpoint,
+            headers: {
+                Authorization: 'Basic ' + btoa(username + ":" + password)
+            },
+            success: function(response) {
+
+                $('.full-result').html(Handlebars.templates.userId({response}));
+                $('.handlebars').on('click', function(e) {
+                    var repoLink = e.target.innerText;
+
+                    $.ajax ({
+                        url: `https://api.github.com/repos/${repoLink}/commits`,
+                        headers: {
+                            Authorization: 'Basic ' + btoa(username + ":" + password)
+                        },
+                        success: function(commits) {
+                            var onlyTen = commits.slice(0,10);
+                            $(e.currentTarget).children(".repo-comments").html(Handlebars.templates.repoData({onlyTen}));
+                        }
+                    });
+                });
+            }
+        });
+    });
+
+    //////////////////////////////////////////////////
+    //                                              //
+    //           LOCAL STORAGE SETUP                //
+    //                                              //
+    // ///////////////////////////////////////////////
+
+    $usernameField.on('input', function(){
+        localStorage.setItem('username', $usernameField.val());
+    });
+    $passwordField.on('input', function(){
+        localStorage.setItem('pass', $passwordField.val());
+    });
+
+    try {
+        $usernameField.val(localStorage.getItem("username"));
+        $passwordField.val(localStorage.getItem("pass"));
+    }
+    catch (err){
+        console.log("Error: ", err);
+    }
+
+}());
---
+++ Week_4/day_3_github_api/practice.js
@@ -0,0 +1,12 @@
+
+var a = 1; //Var has scope availability.  It's availabele only wihtin the scop, e.g. function;
+let b = 2; //Let has block scope.  So it's only available within it's block.  e.g. if statement
+const c = 3;
+// var name = `name`; //backticks for normal strings AND
+
+
+let name = `imad`;
+let lastName = `arain`;
+let fullName = `${name} ${lastName}`;
+
+console.log(fullName);
